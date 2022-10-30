d877be6bbdb66d483938fd7b3fc9d109420df1ad
OttoHartvich <suckots@gmail.com>
2022-10-30 10:41:55 +0100

    Working menu
    -menu
    -start btn
    -quit btn

---
+++ Godot/Autoload/GameEvents/GameEvents.gd
@@ -0,0 +1,4 @@
+extends Node
+
+signal on_start_game
+signal on_game_over
---
+++ Godot/Autoload/Menu/Menu.gd
@@ -0,0 +1,8 @@
+extends CanvasLayer
+
+func _on_StartBtn_button_up():
+	GameEvents.emit_signal("on_start_game")
+	get_tree().change_scene("res://Main.tscn")
+
+func _on_QuitBtn_button_up():
+	get_tree().quit()
