eadf02e85f6cc1ef2700af623040c8ff718586a6
OttoHartvich <suckots@gmail.com>
2022-11-01 08:22:58 +0100

    started button fixed bugs

--- Godot/TextGenerator/StoryBox.gd
+++ Godot/TextGenerator/StoryBox.gd
@@ -1,7 +1,12 @@
 extends Viewport
 
+export(NodePath) onready var container = get_node(container) as MarginContainer
+
+func _on_ready():
+	size = container.rect_size
+	pass
+
 func _process(_delta):
-	if get_node("TextRender"):
-		size = get_node("TextRender").rect_size
+	size.x = container.rect_size.x
 	pass
 
