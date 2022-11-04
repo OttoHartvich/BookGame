24b2090daf8ab693636c0a3237ce08e4c8ac20b1
OttoHartvich <suckots@gmail.com>
2022-11-04 17:50:43 +0100

    constant text render

--- Godot/TextGenerator/TextGenerator.gd
+++ Godot/TextGenerator/TextGenerator.gd
@@ -1,14 +1,16 @@
 extends Node2D
 
 
-export var text_speed = 0.1;
+export var text_speed = 2;
 export(NodePath) onready var text_render = get_node(text_render) as RichTextLabel
 
 func _ready():
 	text_render.percent_visible = 0;
 
 func _process(_delta):
+	var chunk_percentage_value : float = (text_speed+0.00000001) / text_render.text.length() 
+	print(text_speed,text_render.text.length(),chunk_percentage_value)
 	if text_render.percent_visible <= 1:
-		text_render.percent_visible = text_render.percent_visible + text_speed;
+		text_render.percent_visible = text_render.percent_visible + chunk_percentage_value;
 	if text_render.percent_visible > 1:
 		text_render.percent_visible = 1;
