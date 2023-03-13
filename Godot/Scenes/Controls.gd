eebc2ba7ea57d7de038261f51d1103157f0adfc2
Otto Hartvich <70216681+OttoHartvich@users.noreply.github.com>
2023-03-13 18:25:00 +0000

    clean up

--- Godot/Scenes/Controls.gd
+++
@@ -1,8 +0,0 @@
-extends Node
-
-@export_node_path("PopupMenu") var settings_menu
-
-func _input(event):
-	if event.is_action_pressed("escape"):
-		if !settings_menu.is_visible_in_tree():
-			settings_menu.popup_centered()
